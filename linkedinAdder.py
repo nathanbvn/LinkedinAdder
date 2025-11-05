4a9752ca193ad6ec5c5fa3202600f878884a35f4
s4mba4 <94560509+s4mba4@users.noreply.github.com>
2025-11-05 12:17:49 -0500

    V2
    Updated LinkedIn adder script to version 2 with improved functionality and error handling.

--- linkedinAdder.py
+++ linkedinAdder.py
@@ -7,57 +7,70 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 import getpass
+import subprocess
 
-print("---------------------- LINKEDIN ADDER -------------------------")
+print("\n\n---------------------- LINKEDIN ADDER -------------------------")
+print("----------------------------- V2 ------------------------------\n")
 recherche = input("Entrez le domaine des personnes du réseau recherché : ")
+
 id = input("Entrez votre identifiant LinkedIn (e-mail) : ")
 mdp = str(getpass.getpass('Mot de passe : '))
-ajoutsMax = input("Combien de personnes souhaitez-vous ajouter environ ? (1-100)")
+
+ajoutsMax = input("Combien de personnes souhaitez-vous ajouter environ ? (1-100) ")
+print("\n\n Liste des ajouts : \n")
 
 options = Options()
+options.add_argument("--log-level=3") 
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+service = Service(log_output=subprocess.DEVNULL)  
 
 
-
-service = Service()  
 driver = webdriver.Chrome(service=service, options=options)
+
+
 nb_ajouts = 0
 pagenb = 2
 
 try:
    
-    url = "https://www.linkedin.com/search/results/people/?keywords="+recherche+"&page="+str(pagenb)+"&sid=MTY"
+    url = "https://www.linkedin.com/login/fr"
     driver.get(url)
 
    
     element = driver.find_element(By.ID, "username")
     element.send_keys(id)  
-    time.sleep(1)
+    time.sleep(0.5)
 
     element = driver.find_element(By.ID, "password")
     element.send_keys(mdp) 
-    time.sleep(1)
+    time.sleep(0.5)
 
     button = driver.find_element(By.CLASS_NAME, "btn__primary--large.from__button--floating")  
     button.click()
 
 
-
+    driver.get("https://www.linkedin.com/search/results/people/?keywords="+recherche+"&page="+str(pagenb)+"&sid=MTY")
+    
     while int(nb_ajouts) < int(ajoutsMax): 
-        time.sleep(2)
+        time.sleep(5)
         pagenb +=1
-        buttonlist = driver.find_elements(By.CLASS_NAME, "artdeco-button.artdeco-button--2.artdeco-button--secondary.ember-view")
+        buttonlist = driver.find_elements(By.CLASS_NAME, "ember-view")
 
         for i in buttonlist : 
-            aria_label = i.get_attribute("aria-label")
-            if "Invitez" in str(aria_label):
-                i.click()
-                time.sleep(0.5)
-                btnconfirm = driver.find_element(By.CLASS_NAME,"artdeco-button.artdeco-button--2.artdeco-button--primary.ember-view.ml1")
-                btnconfirm.click()
-                name = aria_label.split(" ")
-                print(name[1],name[2],"à été ajouté")
-                nb_ajouts += 1
-                time.sleep(0.5)
+            try :
+                aria_label = i.get_attribute("aria-label")
+                if "Invitez" in str(aria_label):
+                    i.click()
+                    time.sleep(0.5)
+                    btnconfirm = driver.find_element(By.CLASS_NAME,"ml1")
+                    btnconfirm.click()
+                    name = aria_label.split(" ")
+                    print(name[1],name[2],"à été ajouté")
+                    nb_ajouts += 1
+                    time.sleep(0.5)
+            except :
+                pass
         
         driver.get("https://www.linkedin.com/search/results/people/?keywords="+recherche+"&page="+str(pagenb)+"&sid=MTY")
 
